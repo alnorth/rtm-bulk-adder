1884f51a1cec95df95c817d4051720c8db85ed2f
Alasdair North <alnorth@gmail.com>
2013-06-02 11:11:30 +0100

    Logging in process for RTM.

--- js/bulk-add.coffee
+++ js/bulk-add.coffee
@@ -1,5 +1,16 @@
 storageKey = 'data-v2'
 
+ko.bindingHandlers.showModal =
+  init: (element, valueAccessor) ->,
+  update: (element, valueAccessor) ->
+    value = valueAccessor();
+    if ko.utils.unwrapObservable(value)
+      $(element).modal('show')
+      # this is to focus input field inside dialog
+      $("input", element).focus()
+    else
+      $(element).modal('hide')
+
 class List
   constructor: (vm, saved) ->
     console.log saved
@@ -13,12 +24,65 @@ class List
     if !@sending()
       @sending(true)
 
+class Auth
+  constructor: (vm, saved) ->
+    if window.apiKey? and window.sharedSecret?
+      @rtm = new RTM(window.apiKey, window.sharedSecret)
+    else
+      vm.fatalError('apiKey and sharedSecret have not been defined. Have you added a keys.js file as described in the readme?')
+
+    @token = ko.observable(saved.token ? null)
+    @username = ko.observable(saved.username ? "")
+    @loggedIn = ko.observable(false)
+    @tokenExpired = ko.observable(false)
+    @vm = vm
+
+  populateFromFrob: (frob) ->
+    @rtm.apiCall "rtm.auth.getToken", frob: frob, false, (data) =>
+      if data.rsp.stat is "ok"
+        @token(data.rsp.auth.token)
+        @username(data.rsp.auth.user.username)
+
+        @vm.save()
+
+        # Go to this page without frob on the query string so that people won't bookmark it with the frob
+        document.location = document.location.href.split("?")[0];
+      else
+        @vm.fatalError('There was a problem getting a token from Remember the Milk: ' + data.rsp.err.msg)
+
+  checkToken: () ->
+    @rtm.apiCall "rtm.auth.checkToken", auth_token: @token, false, (data) =>
+      if data.rsp.stat is "ok"
+        @loggedIn(true)
+      else
+        @tokenExpired(true)
+
+  ensureToken: () ->
+    frob = @rtm.getParameterByName('frob')
+    if frob isnt ""
+      this.populateFromFrob(frob)
+    else if @token? and @username?
+      this.checkToken()
+
+  authenticateUser: () =>
+    @rtm.authenticateUser()
+
+  toJSON: () ->
+    copy = ko.toJS(this)
+    delete copy.vm
+    delete copy.rtm
+    copy
+
 class ViewModel
   constructor: (saved) ->
     @lists = ko.observableArray()
-    if saved? and saved.lists?
+    @fatalError = ko.observable(null)
+    @auth = new Auth(this, saved.auth ? {})
+    if saved.lists?
       for list in saved.lists
         @lists.push(new List(this, list))
+
+    @loading = ko.computed(() => !(@auth.loggedIn() or @auth.tokenExpired()))
 
   addList: ->
     @lists.push(new List(this))
@@ -29,9 +93,12 @@ class ViewModel
   save: ->
     localStorage.setItem(storageKey, ko.toJSON(this))
 
+
 saved = localStorage.getItem(storageKey)
 if saved?
   saved = JSON.parse(saved)
 else
   saved = {}
-ko.applyBindings(new ViewModel(saved))
+vm = new ViewModel(saved)
+ko.applyBindings(vm)
+vm.auth.ensureToken()
--- js/bulk-add.js
+++ js/bulk-add.js
@@ -1,8 +1,23 @@
 (function() {
-  var List, ViewModel, saved, storageKey,
+  var Auth, List, ViewModel, saved, storageKey, vm,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   storageKey = 'data-v2';
+
+  ko.bindingHandlers.showModal = {
+    init: function(element, valueAccessor) {},
+    update: function(element, valueAccessor) {
+      var value;
+
+      value = valueAccessor();
+      if (ko.utils.unwrapObservable(value)) {
+        $(element).modal('show');
+        return $("input", element).focus();
+      } else {
+        return $(element).modal('hide');
+      }
+    }
+  };
 
   List = (function() {
     function List(vm, saved) {
@@ -29,19 +44,101 @@
 
   })();
 
+  Auth = (function() {
+    function Auth(vm, saved) {
+      this.authenticateUser = __bind(this.authenticateUser, this);
+      var _ref, _ref1;
+
+      if ((window.apiKey != null) && (window.sharedSecret != null)) {
+        this.rtm = new RTM(window.apiKey, window.sharedSecret);
+      } else {
+        vm.fatalError('apiKey and sharedSecret have not been defined. Have you added a keys.js file as described in the readme?');
+      }
+      this.token = ko.observable((_ref = saved.token) != null ? _ref : null);
+      this.username = ko.observable((_ref1 = saved.username) != null ? _ref1 : "");
+      this.loggedIn = ko.observable(false);
+      this.tokenExpired = ko.observable(false);
+      this.vm = vm;
+    }
+
+    Auth.prototype.populateFromFrob = function(frob) {
+      var _this = this;
+
+      return this.rtm.apiCall("rtm.auth.getToken", {
+        frob: frob
+      }, false, function(data) {
+        if (data.rsp.stat === "ok") {
+          _this.token(data.rsp.auth.token);
+          _this.username(data.rsp.auth.user.username);
+          _this.vm.save();
+          return document.location = document.location.href.split("?")[0];
+        } else {
+          return _this.vm.fatalError('There was a problem getting a token from Remember the Milk: ' + data.rsp.err.msg);
+        }
+      });
+    };
+
+    Auth.prototype.checkToken = function() {
+      var _this = this;
+
+      return this.rtm.apiCall("rtm.auth.checkToken", {
+        auth_token: this.token
+      }, false, function(data) {
+        if (data.rsp.stat === "ok") {
+          return _this.loggedIn(true);
+        } else {
+          return _this.tokenExpired(true);
+        }
+      });
+    };
+
+    Auth.prototype.ensureToken = function() {
+      var frob;
+
+      frob = this.rtm.getParameterByName('frob');
+      if (frob !== "") {
+        return this.populateFromFrob(frob);
+      } else if ((this.token != null) && (this.username != null)) {
+        return this.checkToken();
+      }
+    };
+
+    Auth.prototype.authenticateUser = function() {
+      return this.rtm.authenticateUser();
+    };
+
+    Auth.prototype.toJSON = function() {
+      var copy;
+
+      copy = ko.toJS(this);
+      delete copy.vm;
+      delete copy.rtm;
+      return copy;
+    };
+
+    return Auth;
+
+  })();
+
   ViewModel = (function() {
     function ViewModel(saved) {
       this.remove = __bind(this.remove, this);
-      var list, _i, _len, _ref;
+      var list, _i, _len, _ref, _ref1,
+        _this = this;
 
       this.lists = ko.observableArray();
-      if ((saved != null) && (saved.lists != null)) {
-        _ref = saved.lists;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          list = _ref[_i];
+      this.fatalError = ko.observable(null);
+      this.auth = new Auth(this, (_ref = saved.auth) != null ? _ref : {});
+      if (saved.lists != null) {
+        _ref1 = saved.lists;
+        for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+          list = _ref1[_i];
           this.lists.push(new List(this, list));
         }
       }
+      this.loading = ko.computed(function() {
+        return !(_this.auth.loggedIn() || _this.auth.tokenExpired());
+      });
     }
 
     ViewModel.prototype.addList = function() {
@@ -68,6 +165,10 @@
     saved = {};
   }
 
-  ko.applyBindings(new ViewModel(saved));
+  vm = new ViewModel(saved);
+
+  ko.applyBindings(vm);
+
+  vm.auth.ensureToken();
 
 }).call(this);
